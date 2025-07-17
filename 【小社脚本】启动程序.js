c7070f55fa3c42182bfff7f19ee2bf8f6e268690
wengzhenquan <562369706@qq.com>
2025-07-17 22:44:27 +0800

    Add files via upload
    Signed-off-by: wengzhenquan <562369706@qq.com>

--- 【小社脚本】启动程序.js
+++ 【小社脚本】启动程序.js
@@ -947,7 +947,7 @@ function unLock() {
         for (i = 0; i < 2; i++) {
             swipe(dwidth * 5 / 8, dheight * 0.95, dwidth * 5 / 8, dheight * (0.6 - 0.2 * i), 200 * (i + 1));
             wait(() => false, 500)
-            gesture(200 * (2 - i), [dwidth * 3 / 8, dheight * (0.95 - 0.3 * i)], [dwidth * 3 / 8, dheight * (0.3 - 0.1 * i)]);
+            gesture(250 * (2 - i), [dwidth * 3 / 8, dheight * (0.95 - 0.3 * i)], [dwidth * 3 / 8, dheight * (0.3 - 0.1 * i)]);
             wait(() => false, 500)
             
         }
