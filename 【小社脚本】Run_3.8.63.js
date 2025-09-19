f2210f6673c316f75acf76967e97e6a11dce2bea
wengzhenquan <562369706@qq.com>
2025-09-19 08:32:12 +0800

    Add files via upload
    Signed-off-by: wengzhenquan <562369706@qq.com>

--- 【小社脚本】Run_3.8.63.js
+++ 【小社脚本】Run_3.8.63.js
@@ -866,7 +866,7 @@ function 解锁() {
         while (m--) {
             console.warn(`[摸黑]点击第 ${3-m} 个`);
             for (let i = 0; i < 2; i++) {
-                click(dwidth * (0.75 - m * 0.25), dheight * 0.6);
+                click(dwidth * (0.7 - m * 0.2), dheight * 0.6);
                 sleep(500);
             }
             // 关闭提示
